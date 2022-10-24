c120d841eb0aa63893fdc2d50b97458b4d6a0b46
Daria <mar.031281@gmail.com>
2022-10-24 14:40:02 +0300

    add slick slider, add another parts of web-site

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //slider
+$('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+$(document).ready(function(){
+    $('.slider').slick({
+       //settings
+    });
+  });
 
   
